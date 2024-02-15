35739ab15feff26a31b9c869ace3fec6cc1f6f0d
SangramBishwas <info.sangram2003@gmail.com>
2024-02-15 17:31:52 +0600

    sventh commit

--- script/play.js
+++ script/play.js
@@ -6,8 +6,49 @@ function continueGame() {
     //Set a background color on the keyboard
     getAlphabetById.innerHTML = alphabet;
     setBackgroundById(alphabet);
-
 }
+
+function handleKeyboardKeyup(event) {
+    //get expected press key
+    const playerPressed = event.key;
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    //Don't need to use for this Element.innerHTML
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet)
+    if (playerPressed === expectedAlphabet) {
+        const currentScore = getElementValueById('current-score')
+        const newScore = currentScore + 1;
+        setElementValueById('current-score', newScore)
+        //update score:
+        //1. get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        //2. increase the score by one
+        //const newscore = currentScore + 1;
+        //3. Show the new score in display
+        //currentScoreElement.innerText = newscore;
+
+        continueGame();
+        removeBackgroundById(playerPressed);
+    } else {
+        const lifeCount = getElementValueById('life-count');
+        const newLlifeCount = lifeCount - 1;
+        setElementValueById('life-count', newLlifeCount);
+        //Update Life:
+        //1. get the current life count
+        //const currentLifeElement = document.getElementById('life-count')
+        //const currentLifeText = currentLifeElement.innerText;
+        //const currentLifeCount = parseInt(currentLifeText);
+        //2. Reduce the life count
+        //const newLifeCount = currentLifeCount - 1;
+        //3. Show the new life count
+        //currentLifeElement.innerText = newLifeCount;
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyup)
+
 function play() {
     addHiddenByid('home-screen');
     removeHiddenById('play-ground');
--- script/prepare.js
+++ script/prepare.js
@@ -15,12 +15,28 @@ function getARandomAlphabet() {
     // get a random index between 0-25
     const RandomNumber = Math.random() * 25;
     const index = Math.round(RandomNumber);
-    console.log(index);
+    // console.log(index);
     const alphabet = alphabets[index];//.toUpperCase();
     //alphabet.toUpperCase();
     return alphabet;
 }
 function setBackgroundById(elementId){
     const element = document.getElementById(elementId);
-    element.classList.add('bg-warning')
+    element.classList.add('bg-warning');
+}
+
+function removeBackgroundById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-warning');
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementTextValue = element.innerText;
+    const elementValue = parseInt(elementTextValue)
+    return elementValue;
+}
+function setElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
